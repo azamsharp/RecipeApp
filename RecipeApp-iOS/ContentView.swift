77e9b053f1a4cccbfcc4d644dad55b6677520fdb
Mohammad Azam <azamsharp@gmail.com>
2021-09-19 11:35:53 -0500

    Initial Commit

---
+++ RecipeApp-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeApp-iOS
+//
+//  Created by Mohammad Azam on 9/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeApp-iOS/RecipeApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp_iOSApp.swift
+//  RecipeApp-iOS
+//
+//  Created by Mohammad Azam on 9/19/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
